564430be0afcd35914643d22118e74ac6485f35f
sey323 <sey323_nm@yahoo.co.jp>
2024-06-04 23:42:43 +0900

    :sparkles: シンプルなモーダル表示/バックドロップが表示されてモーダルを操作できない不具合あり

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,21 @@
 <template>
-  <a-scene>
-    <a-sky color="#DDEEFF"> </a-sky>
-
-    <a-box
-      color="#4488FF"
-      position="0 0 0"
-      rotation="0 0 0"
-      animation="property: rotation; to: 360 360 0; dur: 4000; easing: linear; loop: true"
-    >
-    </a-box>
-    <!-- ユーザの視点を決定する -->
-    <a-camera position="0 0 2"> </a-camera>
-  </a-scene>
+  <application-title></application-title>
+  <div>
+    <a-scene>
+      <a-box
+        color="#4488FF"
+        position="0 0 0"
+        rotation="0 0 0"
+        animation="property: rotation; to: 360 360 0; dur: 4000; easing: linear; loop: true"
+      >
+      </a-box>
+      <!-- ユーザの視点を決定する -->
+      <a-camera position="0 0 2"> </a-camera>
+    </a-scene>
+    <div class="fixed right-4 bottom-4">
+      <register-modal />
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<style></style>
