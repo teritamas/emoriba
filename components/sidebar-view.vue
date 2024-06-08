efbc76963bb96dd977ae8aac61df0a929e648a3e
sayako21 <63695015+sayako21@users.noreply.github.com>
2024-06-09 00:39:24 +0900

    [追加]イベント日の選択肢 (#14)
    Co-authored-by: sayako21 <>

--- components/sidebar-view.vue
+++ components/sidebar-view.vue
@@ -1,27 +1,4 @@
 <template>
-  <button
-    data-drawer-target="cta-button-sidebar"
-    data-drawer-toggle="cta-button-sidebar"
-    aria-controls="cta-button-sidebar"
-    type="button"
-    class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-  >
-    <span class="sr-only">Open sidebar</span>
-    <svg
-      class="w-6 h-6"
-      aria-hidden="true"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        clip-rule="evenodd"
-        fill-rule="evenodd"
-        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-      />
-    </svg>
-  </button>
-
   <aside
     id="cta-button-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
@@ -30,9 +7,7 @@
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
         <button
-          data-drawer-target="cta-button-sidebar"
-          data-drawer-toggle="cta-button-sidebar"
-          aria-controls="cta-button-sidebar"
+          @click="closeSidebar"
           type="button"
           class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
         >
@@ -56,7 +31,8 @@
           <label
             for="default-search"
             class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-          >Search</label>
+            >Search</label
+          >
           <div class="relative">
             <div
               class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none"
@@ -82,7 +58,7 @@
               type="search"
               class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Search Events"
-            >
+            />
           </div>
         </form>
 
@@ -93,7 +69,8 @@
             />
             <time
               class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-            >リアルタイム</time>
+              >リアルタイム</time
+            >
             <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
               イベント名
             </h3>
@@ -105,7 +82,8 @@
             />
             <time
               class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-            >2024/6/5(水)</time>
+              >2024/6/5(水)</time
+            >
             <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
               ウルトラマンイベント
             </h3>
@@ -117,7 +95,8 @@
             />
             <time
               class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-            >2024/6/4(火)</time>
+              >2024/6/4(火)</time
+            >
             <h3 class="text-md font-semibold text-gray-900 dark:text-white">
               野球の日！
             </h3>
@@ -129,7 +108,8 @@
             />
             <time
               class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-            >2024/6/4(火)</time>
+              >2024/6/4(火)</time
+            >
             <div />
             <small class="text-gray-400 font-normal">10000 memories</small>
           </li>
@@ -139,7 +119,8 @@
             />
             <time
               class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-            >2024/6/4(火)</time>
+              >2024/6/4(火)</time
+            >
             <h3 class="text-md font-semibold text-gray-900 dark:text-white">
               XRハッカソン
             </h3>
@@ -151,7 +132,8 @@
             />
             <time
               class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-            >2024/6/4(火)</time>
+              >2024/6/4(火)</time
+            >
             <h3 class="text-md font-semibold text-gray-900 dark:text-white">
               何にもない日
             </h3>
@@ -199,7 +181,8 @@
             <span class="flex-1 ms-3 whitespace-nowrap">Kanban</span>
             <span
               class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"
-            >Pro</span>
+              >Pro</span
+            >
           </a>
         </li>
         <li>
@@ -221,7 +204,8 @@
             <span class="flex-1 ms-3 whitespace-nowrap">Inbox</span>
             <span
               class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300"
-            >3</span>
+              >3</span
+            >
           </a>
         </li>
         <li>
@@ -319,7 +303,8 @@
         <div class="flex items-center mb-3">
           <span
             class="bg-orange-100 text-orange-800 text-sm font-semibold me-2 px-2.5 py-0.5 rounded dark:bg-orange-200 dark:text-orange-900"
-          >Beta</span>
+            >Beta</span
+          >
           <button
             type="button"
             class="ms-auto -mx-1.5 -my-1.5 bg-blue-50 inline-flex justify-center items-center w-6 h-6 text-blue-900 rounded-lg focus:ring-2 focus:ring-blue-400 p-1 hover:bg-blue-200 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800"
@@ -351,8 +336,16 @@
         <a
           class="text-sm text-blue-800 underline font-medium hover:text-blue-900 dark:text-blue-400 dark:hover:text-blue-300"
           href="#"
-        >Turn new navigation off</a>
+          >Turn new navigation off</a
+        >
       </div>
     </div>
   </aside>
 </template>
+
+<script setup lang="ts">
+const emits = defineEmits(["toggleSidebar"]);
+const closeSidebar = () => {
+  emits("toggleSidebar");
+};
+</script>
