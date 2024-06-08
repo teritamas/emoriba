51bc6234618bb96f6aaebf2b8f057f932783d995
sey323 <sey323_nm@yahoo.co.jp>
2024-06-08 14:51:37 +0900

    :sparkles: 一覧取得APIのクライアントの実装完了

--- pages/index.vue
+++ pages/index.vue
@@ -15,13 +15,40 @@
 <script setup lang="ts">
   const longitude = ref(-1);
   const latitude = ref(-1);
+
   // 2秒ごとに位置情報を取得してログに表示する
   onNuxtReady(() => {
-    setInterval(() => {
+    setInterval(async () => {
       navigator.geolocation.getCurrentPosition((position) => {
         longitude.value = position.coords.longitude;
         latitude.value = position.coords.latitude;
       });
+      // TODO: 実装中
+      // const res = await fetchEmotionalPosts();
     }, 2000);
   });
+
+  // 位置情報から周辺300m以内の投稿を取得する
+  const isLoading = ref(false);
+  const fetchEmotionalPosts = async () => {
+    // 位置情報から周辺300m以内の投稿を取得する
+    isLoading.value = true;
+    // 位置情報はクエリパラメータとして送信する
+    try {
+      const res = await fetch(
+        `api/emotional-posts?$longitude=${longitude.value}&latitude=${latitude.value}`,
+        {
+          method: "get",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return res.json();
+    } catch (error) {
+      return {};
+    } finally {
+      isLoading.value = false;
+    }
+  };
 </script>
