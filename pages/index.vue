ff5d6ec2d5dc372d36c2f6d32bdc3c2e8a785cdd
sey323 <sey323_nm@yahoo.co.jp>
2024-06-08 11:53:48 +0900

    :sparkles: 2秒ごとに位置情報を取得して画面の右上に表示する

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,22 @@
   <div class="fixed right-4 bottom-4">
     <register-modal />
   </div>
+  <div class="absolute top-4 right-4 bg-gray-200 text-xs p-2 rounded-lg">
+    <p>longitude: {{ longitude }}</p>
+    <p>latitude: {{ latitude }}</p>
+  </div>
 </template>
 
-<style></style>
+<script setup lang="ts">
+  const longitude = ref(-1);
+  const latitude = ref(-1);
+  // 2秒ごとに位置情報を取得してログに表示する
+  onNuxtReady(() => {
+    setInterval(() => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        longitude.value = position.coords.longitude;
+        latitude.value = position.coords.latitude;
+      });
+    }, 2000);
+  });
+</script>
