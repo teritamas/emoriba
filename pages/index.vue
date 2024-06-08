2aa7502f30f10947bc17f192c351f3b27485dc14
Ichiro.S <sey323@users.noreply.github.com>
2024-06-08 16:14:29 +0900

    ✨ユーザの位置情報に近い投稿の一覧を取得する (#5)

--- pages/index.vue
+++ pages/index.vue
@@ -15,13 +15,42 @@
 <script setup lang="ts">
   const longitude = ref(-1);
   const latitude = ref(-1);
+
   // 2秒ごとに位置情報を取得してログに表示する
   onNuxtReady(() => {
-    setInterval(() => {
+    setInterval(async () => {
       navigator.geolocation.getCurrentPosition((position) => {
         longitude.value = position.coords.longitude;
         latitude.value = position.coords.latitude;
       });
+      // どちらかが-1の場合は位置情報が取得できていないので処理を実行しない
+      if (longitude.value === -1 || latitude.value === -1) return;
+      const res = await fetchEmotionalPosts();
     }, 2000);
   });
+
+  // 位置情報から周辺300m以内の投稿を取得する
+  const isLoading = ref(false);
+  const fetchEmotionalPosts = async () => {
+    // 位置情報から周辺300m以内の投稿を取得する
+    isLoading.value = true;
+    // 位置情報はクエリパラメータとして送信する
+    try {
+      const res = await fetch(
+        `api/emotional-post?longitude=${longitude.value}&latitude=${latitude.value}`,
+        {
+          method: "get",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(res);
+      return res.json();
+    } catch (error) {
+      return {};
+    } finally {
+      isLoading.value = false;
+    }
+  };
 </script>
