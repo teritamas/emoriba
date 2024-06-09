4dce06a5b3a6821d1e66efd680a05145591db9ff
sey323 <sey323_nm@yahoo.co.jp>
2024-06-09 16:57:08 +0900

    :+1: AR画面で投稿した内容を表示できる

--- components/ar-frame.vue
+++ components/ar-frame.vue
@@ -1,35 +1,34 @@
 <template>
   <a-scene
     vr-mode-ui="enabled: false"
-    loading-screen="enabled: false;"
-    renderer="logarithmicDepthBuffer: true;"
     embedded
-    arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: false;"
+    arjs="sourceType: webcam; debugUIEnabled: false;"
   >
-    <a-entity
-      text="
-      'value: こんにちわ; font: /fonts/hui-msdf.json; font-image: /fonts/hui-msdf.png; align: center; width: 2; negate: false;'
-    "
+    <a-text
+      value="この文章が表示される時、ARは有効です"
+      font="/fonts/hui-msdf.json"
+      font-image="/fonts/hui-msdf.png"
+      position="-3 3 -10"
       look-at="[gps-camera]"
-      rotation="-90 0 0"
-      scale="1 1 1"
+      scale="2 2 2"
+      :gps-entity-place="`latitude: ${deviceLatitude}; longitude: ${deviceLongitude};`"
     />
-    <a-marker preset="hiro">
-      <a-entity
-        v-for="(post, index) in posts"
-        :key="index"
-        :position="'-2 ' + (index * 0.6 - 1) + ' 0'"
-        :text="
-          'value: ' +
-          post.comment +
-          '; font: /fonts/hui-msdf.json; font-image: /fonts/hui-msdf.png; align: center; width: 2; negate: false;'
-        "
-        look-at="[gps-camera]"
-        rotation="-90 0 0"
-        scale="1 1 1"
-      />
-    </a-marker>
-    <a-entity gps-camera rotation-reade />
+    <a-entity
+      v-for="(post, index) in props.posts"
+      :key="index"
+      :text="
+        'value: ' +
+        post.comment +
+        '; font: /fonts/hui-msdf.json; font-image: /fonts/hui-msdf.png; align: center; width: 2; negate: false;'
+      "
+      :position="'-2 ' + (index * 0.6 - 1) + ' -10'"
+      rotation="-90 0 0"
+      scale="2 2 2"
+      look-at="[gps-camera]"
+      :gps-entity-place="`latitude: ${post.coordinates._latitude}; longitude: ${post.coordinates._longitude};`"
+    />
+
+    <a-camera gps-camera rotation-reade />
   </a-scene>
 </template>
 
@@ -40,7 +39,14 @@ const props = defineProps({
   posts: {
     type: Array as PropType<EmotionalPost[]>,
     required: true
+  },
+  deviceLatitude: {
+    type: Number as PropType<number>,
+    required: true
+  },
+  deviceLongitude: {
+    type: Number as PropType<number>,
+    required: true
   }
 })
-const posts = props.posts
 </script>
--- pages/a-frame.vue
+++ pages/a-frame.vue
@@ -2,9 +2,12 @@
   <div class="absolute top-4 right-4 bg-gray-200 text-xs p-2 rounded-lg">
     <p>longitude: {{ longitude }}</p>
     <p>latitude: {{ latitude }}</p>
-    <p>高さ: {{ altitude }}</p>
   </div>
-  <ar-frame :posts="posts" />
+  <ar-frame
+    :posts="posts"
+    :device-latitude="latitude"
+    :device-longitude="longitude"
+  />
 </template>
 
 <script setup lang="ts">
@@ -13,7 +16,6 @@ import type { EmotionalPost } from '~/types/Domain/EmotionalPost'
 
 const longitude = ref(-1)
 const latitude = ref(-1)
-const altitude = ref<number>(-1)
 const posts = ref<EmotionalPost[]>([])
 
 // 2秒ごとに位置情報を取得してログに表示する
@@ -21,22 +23,28 @@ onNuxtReady(() => {
   getCoordinates()
   setInterval(async () => {
     getCoordinates()
+  }, 20000)
+})
+
+/**
+ * 端末の情報を利用して位置情報を取得し、その位置情報を元に感情投稿を取得する
+ */
+function getCoordinates() {
+  console.debug('[getCoordinates] Start')
+  navigator.geolocation.watchPosition(async (position) => {
+    longitude.value = position.coords.longitude
+    latitude.value = position.coords.latitude
+    console.debug('[getCoordinates] Complete!', longitude.value)
     // どちらかが-1の場合は位置情報が取得できていないので処理を実行しない
     if (longitude.value === -1 || latitude.value === -1) {
       return
     }
     const res = (await fetchEmotionalPosts()) as FetchPostResponse
     posts.value = res.posts
-    console.log(posts.value, 'posts')
-  }, 10000)
-})
-function getCoordinates() {
-  navigator.geolocation.getCurrentPosition((position) => {
-    longitude.value = position.coords.longitude
-    latitude.value = position.coords.latitude
-    altitude.value = position.coords.altitude!
+    console.log('posts', posts.value)
   })
 }
+
 // 位置情報から周辺300m以内の投稿を取得する
 const isLoading = ref(false)
 const fetchEmotionalPosts = async () => {
@@ -53,7 +61,6 @@ const fetchEmotionalPosts = async () => {
         }
       }
     )
-    console.log(res)
     return res.json()
   } catch (error) {
     return {}
