f399c7d932368e1206cd137bb9ccce5a47fa3704
Ichiro.S <sey323@users.noreply.github.com>
2024-06-09 22:04:26 +0900

    :sparkles: 吹き出しに文字を表示 (#30)

--- components/ar-frame.vue
+++ components/ar-frame.vue
@@ -5,36 +5,49 @@
     renderer="logarithmicDepthBuffer: true;"
     arjs="sourceType: webcam; debugUIEnabled: false;"
   >
+    <a-assets>
+      <img id="balloon" src="/objects/balloon.svg" />
+    </a-assets>
+
     <a-text
       value="この文章が表示される時、ARは有効です"
       font="/fonts/hui-msdf.json"
       font-image="/fonts/hui-msdf.png"
-      position="-3 3 -10"
       look-at="[gps-camera]"
       scale="2 2 2"
+      negate="false"
+      :position="'0 -0 0'"
       :gps-entity-place="`latitude: ${deviceLatitude}; longitude: ${deviceLongitude};`"
     />
+    <a-plane
+      v-for="(post, index) in props.posts"
+      :key="index"
+      :gps-entity-place="`latitude: ${post.coordinates._latitude}; longitude: ${post.coordinates._longitude};`"
+      :position="'0 ' + (index * 0.6 - 1) + ' 0'"
+      :material="`src: #balloon; color: ; transparent: true; opacity: 0.9; side: double;`"
+      rotation="-90 0 0"
+      look-at="[gps-camera]"
+      :width="`${speechBubbleWidth(post.comment)}`"
+      height="0.45"
+    />
+
     <a-entity
       v-for="(post, index) in props.posts"
       :key="index"
-      :text="
-        'value: ' +
-        post.comment +
-        '; font: /fonts/hui-msdf.json; font-image: /fonts/hui-msdf.png; align: center; width: 2; negate: false;'
-      "
-      :position="'-2 ' + (index * 0.6 - 1) + ' -10'"
+      :text="`value: ${post.comment}; font: /fonts/hui-msdf.json; font-image: /fonts/hui-msdf.png; align: center; width: 2; negate: false; color: ${speechBubbleColor};`"
+      :position="'-2 ' + (index * 0.6 - 0.92) + ' -0.95'"
       rotation="-90 0 0"
       scale="2 2 2"
       look-at="[gps-camera]"
       :gps-entity-place="`latitude: ${post.coordinates._latitude}; longitude: ${post.coordinates._longitude};`"
     />
-
     <a-camera gps-camera rotation-reade />
   </a-scene>
 </template>
 
 <script setup lang="ts">
 import type { EmotionalPost } from '@/types/Domain/EmotionalPost'
+import { ColorPalette } from '@/types/Constant/ColorPalette'
 
 const props = defineProps({
   posts: {
@@ -49,5 +62,17 @@ const props = defineProps({
     type: Number as PropType<number>,
     required: true
   }
+})
+
+const speechBubbleWidth = function (comment: string) {
+  const MIN_WIDTH = 1.4
+  const length = comment.length * 0.2
+
+  return length >= MIN_WIDTH ? length : MIN_WIDTH
+}
+
+const speechBubbleColor = computed(() => {
+  // colorPaletteからランダムで色を選択
+  return ColorPalette[Math.floor(Math.random() * ColorPalette.length)].main
 })
 </script>
---
+++ types/Constant/ColorPalette.ts
@@ -0,0 +1,9 @@
+export const ColorPalette = [
+  { main: '#FBDB4A', shades: ['#FAE073', '#FCE790', '#FADD65', '#E4C650'] },
+  { main: '#F3934A', shades: ['#F7B989', '#F9CDAA', '#DD8644', '#F39C59'] },
+  { main: '#EB547D', shades: ['#EE7293', '#F191AB', '#D64D72', '#C04567'] },
+  { main: '#9F6AA7', shades: ['#B084B6', '#C19FC7', '#916198', '#82588A'] },
+  { main: '#5476B3', shades: ['#6382B9', '#829BC7', '#4D6CA3', '#3E5782'] },
+  { main: '#2BB19B', shades: ['#4DBFAD', '#73CDBF', '#27A18D', '#1F8171'] },
+  { main: '#70B984', shades: ['#7FBE90', '#98CBA6', '#68A87A', '#5E976E'] }
+]
