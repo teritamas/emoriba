8e3080ba0134838ee84c0bc89b2641d3b1756dca
sey323 <sey323_nm@yahoo.co.jp>
2024-06-09 18:15:34 +0900

    :sparkles: A-Frame画面をIndexに統合

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,58 @@
 <template>
-  <div>
-    <button
-      v-if="isSidebarOpen"
-      type="button"
-      class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-      @click="toggleSidebar"
+  <!-- AR.jsの仕様でBody直下である必要がある -->
+  <ar-frame
+    :posts="posts"
+    :device-latitude="latitude"
+    :device-longitude="longitude"
+  />
+  <button
+    v-if="isSidebarOpen"
+    type="button"
+    class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+    @click="toggleSidebar"
+  >
+    <span class="sr-only">Open sidebar</span>
+    <svg
+      class="w-6 h-6"
+      aria-hidden="true"
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <span class="sr-only">Open sidebar</span>
-      <svg
-        class="w-6 h-6"
-        aria-hidden="true"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          clip-rule="evenodd"
-          fill-rule="evenodd"
-          d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-        />
-      </svg>
-    </button>
-    <div>
-      <ar-windows />
-    </div>
-
-    <div class="absolute top-4 left-4 bg-white text-xs p-2 rounded-lg">
-      <button type="button" v-if="!showPicker" @click="showPicker = true">
-        <icon-calendar />
-        {{ selectedEvent }}
-      </button>
-
-      <van-picker
-        v-if="showPicker"
-        title="イベント"
-        confirm-button-text="選択"
-        cancel-button-text="✕"
-        :columns="columns"
-        @confirm="onConfirm"
-        @cancel="onCancel"
-        @change="onChange"
+      <path
+        clip-rule="evenodd"
+        fill-rule="evenodd"
+        d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
       />
-    </div>
+    </svg>
+  </button>
 
-    <div ref="cameraElement">
-      <footer class="fixed bottom-0 left-0 z-20 p-4 w-full">
-        <input-form
-          :camera-area-width="cameraAreaWidth"
-          :camera-area-height="cameraAreaHeight"
-          @register-button-click="postComment"
-        />
-      </footer>
-    </div>
+  <div class="absolute top-4 left-4 bg-white text-xs p-2 rounded-lg">
+    <button type="button" v-if="!showPicker" @click="showPicker = true">
+      <icon-calendar />
+      {{ selectedEvent }}
+    </button>
+
+    <van-picker
+      v-if="showPicker"
+      title="イベント"
+      confirm-button-text="選択"
+      cancel-button-text="✕"
+      :columns="columns"
+      @confirm="onConfirm"
+      @cancel="onCancel"
+      @change="onChange"
+    />
+  </div>
+
+  <div ref="cameraElement">
+    <footer class="fixed bottom-0 left-0 z-20 p-4 w-full">
+      <input-form
+        :camera-area-width="cameraAreaWidth"
+        :camera-area-height="cameraAreaHeight"
+        @register-button-click="postComment"
+      />
+    </footer>
   </div>
 </template>
 
