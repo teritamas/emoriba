dfaf07aa2677c1735ac4651e09f3e42e1b2f85e3
sayako21 <>
2024-06-09 22:00:56 +0900

    [追加]文字数を表示

---
+++ components/icon-send.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg viewBox="0 0 512 512" style="width: 24px; height: 24px; opacity: 1">
+    <g>
+      <polygon
+        style="fill: white"
+        points="121.71,463.73 211.257,394.524 121.71,333.638 	"
+      />
+      <polygon
+        style="fill: white"
+        points="0,216.127 122.938,305.26 465.837,86.043 152.628,326.791 335.73,459.532 512,48.27 	"
+      />
+    </g>
+  </svg>
+</template>
